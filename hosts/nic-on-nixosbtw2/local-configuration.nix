77677cfedfafd0630d6ad4b6e6ae274929a8b640
Nicothekiller <nicolasnaran@gmail.com>
2026-05-31 20:04:44 -0500

    fix: nvidia configs veing fucky for monitors
    Set some env vars from the hyprland wiki and enables an option, now it
    works.

--- hosts/nic-on-nixosbtw2/local-configuration.nix
+++ hosts/nic-on-nixosbtw2/local-configuration.nix
@@ -6,10 +6,18 @@
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.open = true;
   hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.powerManagement.enable = true;
   hardware.graphics.extraPackages = with pkgs; [
     intel-media-driver
     libva-vdpau-driver
   ];
 
   fileSystems."/".options = [ "compress=lzo" ];
+
+  home-manager.users.nic.home.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    LIBVA_DRIVER_NAME = "nvidia";
+  };
 }
