81d53349ce103aff14bb7612274b8b251d85a465
Nicothekiller <nicolasnaran@gmail.com>
2026-06-16 09:21:51 -0500

    chore: update formatting

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,27 +1,30 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 {
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  nix.gc.automatic = true;
-  nix.gc.dates = "daily";
-  nix.gc.options = "--delete-older-than 7d";
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 7d";
+    };
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
 
-  nix.settings = {
-    auto-optimise-store = true;
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
+      substituters = [ "https://yuuhikaze.cachix.org" ];
+      trusted-public-keys = [ "yuuhikaze.cachix.org-1:AtGF4hsoNZahll0Ew3U8fH1CpzKl+OJFPM1tw9qNsYo=" ];
 
-    substituters = [ "https://yuuhikaze.cachix.org" ];
-    trusted-public-keys = [ "yuuhikaze.cachix.org-1:AtGF4hsoNZahll0Ew3U8fH1CpzKl+OJFPM1tw9qNsYo=" ];
-
-    trusted-users = [
-      "root"
-      "nic"
-    ];
+      trusted-users = [
+        "root"
+        "nic"
+      ];
+    };
   };
 
   programs.nix-ld.enable = true;
