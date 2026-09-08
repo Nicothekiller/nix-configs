4dbff3c414ed8dab6c185a1d230ffedf8d6eca91
Nicothekiller <nicolasnaran@gmail.com>
2026-09-07 19:11:12 -0500

    feat: add explicit wireplumber
    i forgot lmao

---
+++ modules/audio.nix
@@ -0,0 +1,18 @@
+{ ... }:
+
+{
+  flake.modules.nixos.audio = { pkgs, ... }: {
+    security.rtkit.enable = true;
+
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      pulse.enable = true;
+      wireplumber.enable = true;
+    };
+
+    environment.systemPackages = with pkgs; [
+      pavucontrol
+    ];
+  };
+}
